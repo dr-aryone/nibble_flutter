82d42903e3eea9cf9dae71b2063879c7ede081c8
edisonywh <edisonywh@gmail.com>
2018-04-22 16:11:45 +0800

    Added FutureBuilder on Nibble

--- lib/components/loading_indicator.dart
+++ lib/components/loading_indicator.dart
@@ -3,19 +3,12 @@ import 'package:flutter/material.dart';
 
 class LoadingIndicator extends StatelessWidget {
 
-  final bool loading;
-
-  LoadingIndicator({this.loading});
-
   @override
   Widget build(BuildContext context) {
     return new Padding(
       padding: const EdgeInsets.all(20.0),
       child: new Center(
-        child: new Opacity(
-          opacity: loading ? 1.0 : 0.0,
-          child: new CircularProgressIndicator(),
-        ),
+        child: new CircularProgressIndicator(),
       ),
     );
   }
