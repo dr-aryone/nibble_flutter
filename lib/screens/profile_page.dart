05c919b8e7c85decab5320cc0d34ea7c69d4877e
edisonywh <edisonywh@gmail.com>
2018-05-06 02:39:38 +0800

    UI for profile page (doesn't work yet)

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -3,21 +3,49 @@ import 'package:flutter/material.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // return new Row(
-    //   children: <Widget>[
-    //     new Expanded(
-    //         child: new Container(
-    //           // height: 250.0,
-    //         color: Colors.red,
-    //         child: new CircleAvatar(
-    //           radius: 100.0,
-    //           backgroundColor: Colors.white,
-    //           child: new Text("EY"),
-    //         ),
-    //       ),
-    //     ),
-    //   ],
-    // );
-    return new Center(child: new Text("Profile Page"));
+    return new Column(
+      children: <Widget>[
+        new Container(
+          child: Image.asset('assets/martian.jpg'),
+        ),
+        new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new InkWell(
+              onTap: () => print("Clicked"),
+              child: new ListTile(
+                leading: new Icon(Icons.feedback),
+                title: new Text("Feedback"),
+                trailing: new Switch(
+                  activeColor: Theme.of(context).primaryColor,
+                  value: true,
+                  onChanged: (bool newValue) {
+                    print("Something");
+                  },
+                ),
+              ),
+            ),
+            new Divider(),
+            new InkWell(
+              onTap: () => print("Clicked"),
+              child: new ListTile(
+                leading: new Icon(Icons.info),
+                title: new Text("About"),
+              ),
+            ),
+            new Divider(),
+            new InkWell(
+              onTap: () => print("Clicked"),
+              child: new ListTile(
+                leading: new Icon(Icons.feedback),
+                title: new Text("Feedback"),
+              ),
+            ),
+            new Divider(),
+          ],
+        ),
+      ],
+    );
+    // return new Center(child: new Text("Profile Page"));
   }
 }
